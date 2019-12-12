2b120b6378ca09dcc8220fb9ff2f650eeeff10af
Avihay Barnholtz <avihay107@gmail.com>
2019-12-12 14:55:35 +0200

    Functions_GUI update

--- src/Ex1/Functions_GUI.java
+++ src/Ex1/Functions_GUI.java
@@ -2,22 +2,14 @@ package Ex1;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.Random;
-
-import javax.security.auth.x500.X500Principal;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -30,18 +22,18 @@ import org.json.simple.parser.ParseException;
  * The Class Functions_GUI.
  */
 public class Functions_GUI implements functions {
-	
+
 	/** The Functions. */
 	private ArrayList<function> Functions;
-	
+
 	/**
-	 * Instantiates a new functions GUI.
+	 * Instantiates a new Functions_GUI object.
 	 */
 	public Functions_GUI() {
 		Functions=new ArrayList<>();
-		
+
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see java.util.Collection#size()
 	 */
@@ -171,8 +163,9 @@ public class Functions_GUI implements functions {
 	@Override
 	public void saveToFile(String file) throws IOException {
 		BufferedWriter bufferedwriter = new BufferedWriter(new FileWriter(file));
-		for(int i=0;i<Functions.size();i++) {
-			bufferedwriter.write(Functions.get(i).toString());
+		Iterator<function> itr=this.iterator();
+		while(itr.hasNext()) {
+			bufferedwriter.write(itr.next().toString());
 			bufferedwriter.newLine();
 		}
 		bufferedwriter.close();
@@ -184,66 +177,62 @@ public class Functions_GUI implements functions {
 	 */
 	@Override
 	public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
+
 		StdDraw.setCanvasSize(width,height);
-		double[] x=new double[resolution+1];
-		double[][] y=new double[Functions.size()][resolution+1];
 		double ranger=(rx.get_max()-rx.get_min())/resolution;
-		for(int i=1;i<resolution+1;i++) {
-			x[i]=rx.get_min()+(ranger*i);
-		}
-		for(int i=0;i<Functions.size();i++) {
-			for(int j=1;j<resolution+1;j++) {		
-				y[i][j]=Functions.get(i).f(x[j]);
-			}
-		}
+		int RangeY=(int)((ry.get_max()-ry.get_min())/20);
+		int RangeX=(int)((rx.get_max()-rx.get_min())/20);
+
 		StdDraw.setXscale(rx.get_min(),rx.get_max());
 		StdDraw.setYscale(ry.get_min(),ry.get_max());
-		
-		
+
 		//Setting the horizontal and vertical lines
 		StdDraw.setPenColor(Color.LIGHT_GRAY);
-		for (double i = rx.get_min(); i <=rx.get_max(); i++) {
+		for (double i = rx.get_min(); i <=rx.get_max(); i+=RangeX) {
 			StdDraw.line(i,ry.get_min(),i,ry.get_max());
 		}
-		
-		for (double i =ry.get_min(); i <=ry.get_max(); i=i+1) {
+
+		for (double i =ry.get_min(); i <=ry.get_max(); i=i+=RangeY) {
 			StdDraw.line(rx.get_min(),i,rx.get_max(), i);
 		}
-		
+
 		//Paints the Y and X axises with numbers
 		StdDraw.setPenColor(Color.BLACK);
 		StdDraw.setPenRadius(0.005);
 		StdDraw.line(rx.get_min(),0,rx.get_max(),0);
 		StdDraw.line(0,ry.get_min(),0,ry.get_max());
 		StdDraw.setFont(new Font("TimesRoman", Font.BOLD, 15));
-		int RangeY=(int)((ry.get_max()-ry.get_min())/20);
-		int RangeX=(int)((rx.get_max()-rx.get_min())/20);
-		
-		for (int i =RangeX,j=-RangeX;( i <=rx.get_max()||j>=rx.get_min()); i+=RangeX,j-=RangeX) {
+
+		for (int i =0,j=0;( i <=rx.get_max()||j>=rx.get_min()); i+=RangeX,j-=RangeX) {
 			if(i<=rx.get_max()) StdDraw.text(i-0.30, -0.30, Integer.toString(i));
 			if(j>=rx.get_min())StdDraw.text(j-0.30, -0.30, Integer.toString(j));
 		}	
 		//y numbers
 
-		for (int i =RangeY,j=-RangeY;( i <=ry.get_max()||j>=ry.get_min()); i+=RangeY,j-=RangeY) {
+		for (int i =0,j=0;( i <=ry.get_max()||j>=ry.get_min()); i+=RangeY,j-=RangeY) {
 			if(i<=ry.get_max()) StdDraw.text(0-0.30, i-0.30,Integer.toString(i));
 			if(j>=ry.get_min()) StdDraw.text(0-0.30, j-0.30,Integer.toString(j));
 		}
-		
-		 
-		
+
+
+
 
 		for (int i = 0;i<Functions.size(); i++) {
 			Color RandomColor= new Color((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
 			StdDraw.setPenColor(RandomColor);
-			for(int j=1;j<resolution;j++) {
-				StdDraw.line(x[j], y[i][j], x[j+1], y[i][j+1]);
+			for(int j=1;j<resolution-1;j++) {
+				double x1=rx.get_min()+(ranger*j);
+				double x2=rx.get_min()+(ranger*(j+1));
+				double value1=(double)Math.round((Functions.get(i).f(x1))*100000d)/100000d;
+				double value2=(double)Math.round((Functions.get(i).f(x2))*100000d)/100000d;
+				StdDraw.line(x1,value1, x2,value2);
 			}
 
 		}
 		StdDraw.setPenColor(Color.RED);
 		StdDraw.setPenRadius(0.01);
-		StdDraw.point(x[resolution/2], 0);
+		StdDraw.point(0, 0);
+		
 
 
 
@@ -254,7 +243,7 @@ public class Functions_GUI implements functions {
 	 * @see Ex1.functions#drawFunctions(java.lang.String)
 	 */
 	@Override
-	public void drawFunctions(String json_file) throws IOException,ParseException  {
+	public void drawFunctions(String json_file) {
 		int Width=0,Height=0,Resolution=0;//our paramateres
 		Range Range_x,Range_y;
 		double[][] Ranges=new double[2][2];
@@ -266,19 +255,19 @@ public class Functions_GUI implements functions {
 			/*Initilaizing The parameters from the json file
 			 first we must check if the value is not empty and has null value
 			 and after this we convert those values into our parameters*/
-			
+
 			if(Jobj.get("Width")!=null&&Jobj.get("Height")!=null&&Jobj.get("Resolution")!=null) {
 				Width=Math.toIntExact((long)Jobj.get("Width"));
 				Height=Math.toIntExact((long)Jobj.get("Height"));
 				Resolution=Math.toIntExact((long)Jobj.get("Resolution"));
 				System.out.println(Width+" "+Height+" "+Resolution);
 			}
-			
-			
+
+
 			/*Doing the same thing to the Ranges. 
 			 because range object has tw values,we treat him like an array
 			 and after this we pass those array's values to the ranges*/
-			
+
 			JSONArray xValues,yValues;
 			if(Jobj.get("Range_X")!=null&&Jobj.get("Range_Y")!=null) {
 				xValues=(JSONArray)Jobj.get("Range_X");
